fec964fadf49b44312a88ac8a1dda3ee63848a35
p-x9 <chi.iosdev@gmail.com>
2021-03-09 23:49:28 +0900

    Support mouse control

--- pock-timer-widget/Sources/TimerWidget.swift
+++ pock-timer-widget/Sources/TimerWidget.swift
@@ -20,3 +20,28 @@ class TimerWidget: PKWidget {
     }
 
 }
+
+extension TimerWidget: PKScreenEdgeMouseDelegate {
+    private func shouldHighlight(for location: NSPoint, in view: NSView) -> Bool {
+        timerView.convert(timerView.bounds, to: view).contains(location)
+    }
+
+    func screenEdgeController(_ controller: PKScreenEdgeController, mouseEnteredAtLocation location: NSPoint, in view: NSView) {
+        timerView.isHighlighted = shouldHighlight(for: location, in: view)
+    }
+
+    func screenEdgeController(_ controller: PKScreenEdgeController, mouseMovedAtLocation location: NSPoint, in view: NSView) {
+        timerView.isHighlighted = shouldHighlight(for: location, in: view)
+    }
+
+    func screenEdgeController(_ controller: PKScreenEdgeController, mouseClickAtLocation location: NSPoint, in view: NSView) {
+        timerView.isHighlighted = shouldHighlight(for: location, in: view)
+        if timerView.isHighlighted {
+            timerView.state == .stopping ? timerView.start(): timerView.stop()
+        }
+    }
+
+    func screenEdgeController(_ controller: PKScreenEdgeController, mouseExitedAtLocation location: NSPoint, in view: NSView) {
+        timerView.isHighlighted = false
+    }
+}
