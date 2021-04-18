0c73fea4e0b4d4de20fad44d827bc7d7b2b25cf6
p-x9 <chi.iosdev@gmail.com>
2021-04-18 17:01:14 +0900

    Fix background color when in light mode

---
+++ pock-timer-widget/Sources/Extension/NSColor.swift
@@ -0,0 +1,14 @@
+//
+//  NSColor.swift
+//  pock-timer-widget
+//
+//  Created by p-x9 on 2021/04/18.
+//  
+//
+
+import Cocoa
+
+extension NSColor {
+    static let touchBarBackgroundColor = NSColor(red: 54 / 256, green: 54 / 256, blue: 54 / 256, alpha: 1.0)
+
+}
